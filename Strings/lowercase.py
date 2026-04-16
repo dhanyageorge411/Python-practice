de435f166ff07c68141eb0744cbcc6a563d6044f
dhanyageorge411 <dhanya.george@gmail.com>
2026-04-16 23:30:55 +0530

    Added lowercase file

---
+++ Strings/lowercase.py
@@ -0,0 +1,3 @@
+#Implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
+name = input("Enter the text in upper case: ")
+print("Lower case text:",name.lower())
