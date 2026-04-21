df17d25fdfce00392d9846a34c8835b811428ef3
dhanyageorge411 <dhanya.george@gmail.com>
2026-04-21 21:34:15 +0530

    Added compare file

---
+++ Conditionals/compare.py
@@ -0,0 +1,13 @@
+# Input numbers from users 
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+#Compare using if 
+
+if x<y :
+    print("First number is less than second")
+elif x>y:
+    print("First is greater than second")  
+else:
+    print("First is equal to second")      
